8e1fec23037fc73c80b85b6a7fb76e1bd65b5b0a
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-07-17 19:26:37 -0400

    [Neeraj] Implementing web kit web view with control buttons

--- WebKitExample/ViewController.swift
+++ WebKitExample/ViewController.swift
@@ -7,14 +7,61 @@
 //
 
 import UIKit
-
+import WebKit
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var webKit: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.loadFromURL()
+        //self.loadFromFile()
     }
-
-
+    
+    func loadFromString(){
+        let htmlStr = """
+                            <h1>Hello World</h1>
+                            <h2>Hello World</h2>
+                            <h3>Hello World</h3>
+                            <h4>Hello World</h4>
+                            <h5>Hello World</h5>
+                            <h6>Hello World</h6>
+                      """
+        webKit.loadHTMLString(htmlStr, baseURL: nil)
+    }
+    
+    func loadFromURL(){
+        let url = URL(string: "https://www.lambtoncollege.ca")
+        let urlReq = URLRequest(url: url!)
+        webKit.load(urlReq)
+    }
+    
+    func loadFromFile(){
+        let localfilepath = Bundle.main.url(forResource: "about", withExtension: "html")
+        let myRequest = URLRequest(url: localfilepath!)
+        webKit.load(myRequest)
+    }
+    
+    @IBAction func webBrowserAction(_ sender: UIBarButtonItem) {
+        switch sender.tag {
+        case 0:
+            webKit.reloadFromOrigin()
+        case 1:
+            if webKit.canGoForward{
+                webKit.goForward()
+            }
+        case 2:
+            if webKit.canGoBack{
+                webKit.goBack()
+            }
+        case 3:
+            webKit.reload()
+        default:
+            print("Nothing to do")
+        }
+    }
+    
+    
 }
 
